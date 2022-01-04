42f6caa93cff073e349abed3b67c7d73521d8e91
Attakorn <attakorn21112@gmail.com>
2022-01-04 17:33:08 +0700

    fix liff

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -77,8 +77,6 @@ export default {
     msg: String
   },
   beforeCreate() {
-    //1656343498-yxPX147d netlify
-    //1656343498-aWZNJzxy heroku
     liff.init({ liffId: "1656343498-pwzGanJe" }, () => {
       if (liff.isLoggedIn()) {
         liff.getProfile()
@@ -91,24 +89,6 @@ export default {
         liff.login();
       }
     });
-  },
+  }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
